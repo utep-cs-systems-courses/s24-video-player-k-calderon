6348fc5b7f3d738a060989fcb0fa1b39de829c75
Kelcey Calderon <kcalderon@miners.utep.edu>
2024-04-28 13:08:41 -0600

    myVideoPlayer rework

--- myVideoPlayer.py
+++ myVideoPlayer.py
@@ -1,88 +1,43 @@
 #!/usr/bin/env python3
 
 import threading
-import cv2
-import numpy as np
-import base64
-import queue
 import log as l
-import sys
-import os
 from BlockingQueue import BlockingQueue
 from myExtractFrames import extractFrames
 from myDisplayFrames import displayFrames
+from myConvertToGrayscale import colorToGrayscale
 
-# take and parse user arguments
-# filename
-# grayscale
 
-def main2():
-    filename = "clip.mp4"
-    maxFrames = 72
-    queueCapacity = 10
-
-    grayscaleHandler = BlockingQueue(queueCapacity)
-    displayHander = BlockingQueue(queueCapacity)
-    # Create thread to run Extracting frames in parallel
-    extractWorker = threading.Thread(target=extractFrames, args=(filename,coloredFramesQueue,10000))
-    extractWorker.start()
-
-    # Create thread to run color to grayscale function in parallel
-    #grayscaleWorker = threading.Thread(target=colorToGrayscale,args=(coloredFramesQueue,grayscaleFramesQueue))
-    #grayscaleWorker.start()
-
-    # Create thread to run display frames in parallel 
-    displayWorker = threading.Thread(target=displayFrames,args=(grayscaleFramesQueue,))
-    displayWorker.start()
-
-    # Join all created threads to main thread
-    extractWorker.join()
-    grayscaleWorker.join()
-    displayWorker.join()
 def main():
     filename = 'clip.mp4'
-    maxFrames = 72
-    queue_capacity = 10
+    maxFramesToLoad = 100
 
-    # Create a shared BlockingQueue
-    extractionQueue = BlockingQueue(queue_capacity)
-    l.debug("Initialized extractionQueue", extractionQueue)
+    coloredFramesQueue = BlockingQueue(10)
+    grayscaleFramesQueue = BlockingQueue(10)
 
-    # Fork a process
-    pid = os.fork()
+    # create and run the frame extraction thread
+    extractThread = threading.Thread(target=extractFrames, args=(filename,coloredFramesQueue, maxFramesToLoad))
+    extractThread.start()
 
-    if pid == 0:
-        l.debug("Child process: responsible for extracting frames")
-        # Child process: responsible for extracting frames
-        l.debug("Child > passing exQ to extractFrames", extractionQueue)
-        extractFrames(filename, extractionQueue, maxFrames)
-    else:
-        # Parent process forks again to create another process for display
-        pid = os.fork()
-        if pid == 0:
-            l.debug("Second child process: responsible for displaying frames")
-            # Second child process: responsible for displaying frames
-            l.debug("Child > passing exQ to displayFrames", extractionQueue)
-            displayFrames(extractionQueue)
-        else:
-            l.debug("Parent process waits for child processes to finish")
-            # Parent process waits for child processes to finish
-            os.wait()
+    # create and run the grayscale converter thread
+    grayscaleThread = threading.Thread(target=colorToGrayscale,args=(coloredFramesQueue,grayscaleFramesQueue))
+    grayscaleThread.start()
+
+    # create and run the frame displaying thread
+    displayThread = threading.Thread(target=displayFrames,args=(grayscaleFramesQueue,))
+    displayThread.start()
+
+    '''
+    join() blocks main until the thread it's attached to is done.
+    join can be thought of as a "wait"
+    so this is making sure all the threads finish before the main progresses
+    in this case, there's not really anyhing left to do, but join is useful if there was
+    '''
+    extractThread.join()
+    grayscaleThread.join()
+    displayThread.join()
+    l.log("All done")
+    
 
 if __name__ == "__main__":
     main()
-
-
-'''
-# filename of clip to load
-filename = 'clip.mp4'
-
-# shared queue  
-extractionQueue = BlockingQueue(10)
-
-# extract the frames
-extractFrames(filename,extractionQueue, 72)
-
-# display the frames
-displayFrames(extractionQueue)
-'''
