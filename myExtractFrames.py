b3a237f856bd74a009a5c533c1b6e1317a07cae0
Kelcey Calderon <kcalderon@miners.utep.edu>
2024-04-28 13:30:10 -0600

    more myExtractFrames cleanup

--- myExtractFrames.py
+++ myExtractFrames.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
 import cv2
-import base64
 
 def extractFrames(fileName, coloredFramesQueue, maxFramesToLoad=9999):
-    # Initialize frame count 
+    # frame counter
     count = 0
 
     # open video file
@@ -18,17 +17,18 @@ def extractFrames(fileName, coloredFramesQueue, maxFramesToLoad=9999):
         # get a jpg encoded frame
         success, jpgImage = cv2.imencode('.jpg', image)
 
+        # break if the frame couldn't be read
         if not success:
             break
 
-        #encode the frame as base 64 to make debugging easier
-        jpgAsText = base64.b64encode(jpgImage)
-
         # add the frame to the buffer
         coloredFramesQueue.put(image)
+
+        # extract the next frame and get ready for the next loop
         success,image = vidcap.read()
         print(f'Reading frame {count} {success}')
         count += 1
 
     print('Frame extraction complete')
-    coloredFramesQueue.put(None)      # End of frame
+    # signal frame extraction is complete with None
+    coloredFramesQueue.put(None)
